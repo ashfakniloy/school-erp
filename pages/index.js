17c970b2876ad13edfe3b0a36f32dd3826b1938c
Ashfak Niloy <ashfakniloy56@gmail.com>
2022-08-26 17:59:00 +0600

    changed index

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Login from "../components/Login";
 import Router, { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useSession } from "next-auth/react";
+import { role } from "../config";
+import Loader from "../components/Layout/Loader";
 
 function Home() {
   const router = useRouter();
-  const { data } = useSession();
-  const { role } = data ? data.user : "";
+  // const { data } = useSession();
+  // const { role } = data ? data.user : "";
 
   // useEffect(() => {
   //   if (role) {
